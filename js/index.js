52d4fe3e52f30858f00197ab8ab2a75d2eef6bf5
KVKYES <winstontroysings@gmail.com>
2016-02-15 14:21:01 -0500

    Many attempts at FAQ

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Tip: You can either hide the answer on page load with js or use display:none in css
+
+
+$("#question1").click(function(){
+    $("#answer1").slideToggle();
+});
+​
+$("#question2").click(function(){
+    $("#answer2").slideToggle();
+});
+​
+$("#question3").click(function(){
+    $("#answer3").slideToggle();
+});
